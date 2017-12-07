069271d99dd61e01cf35ce4010d9798d1ae8d340
Oliver Pabst <oliver.pabst@outlook.de>
2017-12-07 09:30:56 +0100

    - eingehende Änderungen gemerged - Kopieren des sortierten Teilarrays an korrekte Position verschoben

--- src/NaturalMergeSort.java
+++ src/NaturalMergeSort.java
@@ -1,59 +1,59 @@
 import java.util.*;
 
 /**
-Eventuell in jeder Iteration runs suchen? Iteration abbrechen wenn nur noch ein run vorhanden ist
+ Eventuell in jeder Iteration runs suchen? Iteration abbrechen wenn nur noch ein run vorhanden ist
  */
 
 public class NaturalMergeSort {
-        private static int n;
+    private static int n;
 
-        public static void main(String [] args) {
+    public static void main(String [] args) {
 
-            int[] test1 = {1, 5, 2, 4, 3};
-            System.out.println("Test1: " + Arrays.toString(test1));
-            naturalMergeSort(test1);
+        int[] test1 = {1, 5, 2, 4, 3};
+        System.out.println("Test1: " + Arrays.toString(test1));
+        naturalMergeSort(test1);
 
-            int[] test2 = {17, 15, 14, 12, 13, 11, 9, 10, 5};
-            System.out.println("Test2: " + Arrays.toString(test2));
-            naturalMergeSort(test2);
+        int[] test2 = {17, 15, 14, 12, 13, 11, 9, 10, 5};
+        System.out.println("Test2: " + Arrays.toString(test2));
+        naturalMergeSort(test2);
 
-            int[] test3 = {4, 8, 5, 7};
-            System.out.println("Test3: " + Arrays.toString(test3));
-            naturalMergeSort(test3);
-            //System.out.println(Arrays.toString(test1));
-        }
+        int[] test3 = {4, 8, 5, 7};
+        System.out.println("Test3: " + Arrays.toString(test3));
+        naturalMergeSort(test3);
+        //System.out.println(Arrays.toString(test1));
+    }
 
-        private static Queue<Integer> findRuns(int[] A) {
-            Queue<Integer> runs = new ArrayDeque<>();
-            int low = 0;
-            int high = 1;
-            while (high <= n) {
-                while(high < n && A[high-1] < A[high]) {
-                    high++;
-                }
-                // Run beenden
-                runs.add(high-1);
-                // Neuer low-Wert liegt auf dem run-verletzenden Wert
-                low = high;
+    private static Queue<Integer> findRuns(int[] A) {
+        Queue<Integer> runs = new ArrayDeque<>();
+        int low = 0;
+        int high = 1;
+        while (high <= n) {
+            while(high < n && A[high-1] < A[high]) {
                 high++;
             }
-
-            return runs;
+            // Run beenden
+            runs.add(high-1);
+            // Neuer low-Wert liegt auf dem run-verletzenden Wert
+            low = high;
+            high++;
         }
 
-        private static void naturalMergeSort(int[] A) {
-            n = A.length;
-            Queue<Integer> runs = findRuns(A);
+        return runs;
+    }
 
-            // merge runs
-            
+    private static void naturalMergeSort(int[] A) {
+        n = A.length;
+        Queue<Integer> runs = findRuns(A);
 
-            // In while-Schleife gefundene Runs abarbeiten
-            while(runs.size() > 1) {
-                // Nach jeder Iteration erneut bei 0 initialisieren
-                int start = 0;
-                // Runs paarweise in For-Schleife abarbeiten
-                if(runs.size() > 1) {
+        // merge runs
+
+
+        // In while-Schleife gefundene Runs abarbeiten
+        while(runs.size() > 1) {
+            // Nach jeder Iteration erneut bei 0 initialisieren
+            int start = 0;
+            // Runs paarweise in For-Schleife abarbeiten
+            if(runs.size() > 1) {
                 int runHigh1 = runs.poll();
                 int runHigh2 = runs.poll();
                 int[] B = new int[(runHigh1 - start) + (runHigh2 - runHigh1 + 1)];
@@ -88,22 +88,22 @@ public class NaturalMergeSort {
                         }
                     }
                 }
-                } else {
-                    // letzten Run verwerfen
-                    runs.poll();
-                }
-                // B nach A kopieren
+                // B nach A kopieren nach Merge des runs
                 for(int l = start; l < runHigh2; l++) {
                     A[l] = B[l];
                 }
-                runs = findRuns(A);
-                
-
-
-
-                // run ist per Definition sortiert, also neue Grenzen erneut einfügen mit
-
+            } else {
+                // letzten Run verwerfen
+                runs.poll();
             }
-            System.out.println("Sortiertes Ergebnis: " + Arrays.toString(A));
+            runs = findRuns(A);
+
+
+
+
+            // run ist per Definition sortiert, also neue Grenzen erneut einfügen mit
+
         }
+        System.out.println("Sortiertes Ergebnis: " + Arrays.toString(A));
+    }
 }
